04faca9584299d3845aee57589a32aeafaae568b
Edson Cepeda <raulcepedac@hotmail.com>
2021-03-13 15:00:15 -0600

    Init discord bot

---
+++ MOB/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'embed') {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Playlist saved correctly by ${msg.author.username}!`)
+        .setThumbnail(msg.author.displayAvatarURL())
+        .setColor(0x49d1cd)
+        .addField("Name", "YHLQMDLG", true)
+        .addField("Playlist ID", "1", true)
+        .addField("URL", "https://www.youtube.com/playlist?list=PL8va0X5IHAfOfX2Hnnv42sTk_PpDRMFZ0")
+        .setFooter("Use ::help command to see MOB's abilities.")
+    msg.channel.send(embed);
+        
+    // msg.reply(msg.author.displayAvatarURL());
+  }
+});
+
+client.login("ODA5NjE5MTU3MjM4NTQ2NDYy.YCXumQ.SAPTuxD25VrQIIiwNxkY2oXirOo");
